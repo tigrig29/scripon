41626da3ff8abf284f042c09c63373b36a4d255a
tigrig <tigrig.crex@gmail.com>
2019-03-26 17:31:30 +0900

    設定サイドバー レイアウト基礎作成

--- components/SettingArea.vue
+++ components/SettingArea.vue
@@ -1,44 +1,35 @@
 <template>
-  <section id="setting-sidebar">
-    <div class="sidebar">
-      <p>コメント削除</p>
-      <p>シナリオ先頭に追加</p>
-      <p>シナリオ末尾に追加</p>
-      <p>行頭に追加</p>
-      <p>行末に追加</p>
-      <p>詳細な置換</p>
-      <p>コメント削除</p>
-      <p>シナリオ先頭に追加</p>
-      <p>シナリオ末尾に追加</p>
-      <p>行頭に追加</p>
-      <p>行末に追加</p>
-      <p>詳細な置換</p>
-      <p>コメント削除</p>
-      <p>シナリオ先頭に追加</p>
-      <p>シナリオ末尾に追加</p>
-      <p>行頭に追加</p>
-      <p>行末に追加</p>
-      <p>詳細な置換</p>
-      <p>コメント削除</p>
-      <p>シナリオ先頭に追加</p>
-      <p>シナリオ末尾に追加</p>
-      <p>行頭に追加</p>
-      <p>行末に追加</p>
-      <p>詳細な置換</p>
-      <p>コメント削除</p>
-      <p>シナリオ先頭に追加</p>
-      <p>シナリオ末尾に追加</p>
-      <p>行頭に追加</p>
-      <p>行末に追加</p>
-      <p>詳細な置換</p>
-      <p>コメント削除</p>
-      <p>シナリオ先頭に追加</p>
-      <p>シナリオ末尾に追加</p>
-      <p>行頭に追加</p>
-      <p>行末に追加</p>
-      <p>詳細な置換</p>
+  <aside>
+    <div class="settings-container">
+      <h2 class="settings-title">設定</h2>
+      <div class="settings-delete-comment settings-form">
+        <label>コメント削除</label>
+      </div>
+      <div class="settings-add-first settings-form">
+        <label>シナリオ先頭に追加</label>
+      </div>
+      <div class="settings-add-last settings-form">
+        <label>シナリオ末尾に追加</label>
+      </div>
+      <div class="settings-add-line-head settings-form">
+        <label>行頭に追加</label>
+      </div>
+      <div class="settings-add-line-head settings-form">
+        <label>行末に追加</label>
+      </div>
+      <div class="settings-add-line-end settings-form">
+        <label>詳細な置換</label>
+      </div>
     </div>
-  </section>
+  </aside>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.settings-container {
+  border: 3px solid #efefef;
+  .settings-title {
+    margin: 10px auto;
+    text-align: center;
+  }
+}
+</style>
