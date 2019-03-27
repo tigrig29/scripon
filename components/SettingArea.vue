613235b12405cb3f9e82009a56ab09d912c4ff14
tigrig <tigrig.crex@gmail.com>
2019-03-27 13:27:19 +0900

    設定サイドバー レイアウト基礎変更

--- components/SettingArea.vue
+++ components/SettingArea.vue
@@ -2,34 +2,113 @@
   <aside>
     <div class="settings-container">
       <h2 class="settings-title">設定</h2>
-      <div class="settings-delete-comment settings-form">
-        <label>コメント削除</label>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01"
+            >コメント削除</label
+          >
+        </div>
+        <select id="inputGroupSelect01" class="custom-select">
+          <option :selected="settings.deleteComments" value="true">する</option>
+          <option :selected="!settings.deleteComments" value="false"
+            >しない</option
+          >
+        </select>
       </div>
-      <div class="settings-add-first settings-form">
-        <label>シナリオ先頭に追加</label>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01"
+            >シナリオ先頭に追加</label
+          >
+        </div>
+        <select id="inputGroupSelect01" class="custom-select">
+          <option :selected="settings.deleteComments" value="true">する</option>
+          <option :selected="!settings.deleteComments" value="false"
+            >しない</option
+          >
+        </select>
       </div>
-      <div class="settings-add-last settings-form">
-        <label>シナリオ末尾に追加</label>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01"
+            >シナリオ末尾に追加</label
+          >
+        </div>
+        <select id="inputGroupSelect01" class="custom-select">
+          <option :selected="settings.deleteComments" value="true">する</option>
+          <option :selected="!settings.deleteComments" value="false"
+            >しない</option
+          >
+        </select>
       </div>
-      <div class="settings-add-line-head settings-form">
-        <label>行頭に追加</label>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01"
+            >行頭に追加</label
+          >
+        </div>
+        <select id="inputGroupSelect01" class="custom-select">
+          <option :selected="settings.deleteComments" value="true">する</option>
+          <option :selected="!settings.deleteComments" value="false"
+            >しない</option
+          >
+        </select>
       </div>
-      <div class="settings-add-line-head settings-form">
-        <label>行末に追加</label>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01"
+            >行末に追加</label
+          >
+        </div>
+        <select id="inputGroupSelect01" class="custom-select">
+          <option :selected="settings.deleteComments" value="true">する</option>
+          <option :selected="!settings.deleteComments" value="false"
+            >しない</option
+          >
+        </select>
       </div>
-      <div class="settings-add-line-end settings-form">
-        <label>詳細な置換</label>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01"
+            >詳細な置換</label
+          >
+        </div>
+        <select id="inputGroupSelect01" class="custom-select">
+          <option :selected="settings.deleteComments" value="true">する</option>
+          <option :selected="!settings.deleteComments" value="false"
+            >しない</option
+          >
+        </select>
       </div>
     </div>
   </aside>
 </template>
 
+<script>
+export default {
+  computed: {
+    settings() {
+      return {
+        deleteComments: true
+      }
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 .settings-container {
   border: 3px solid #efefef;
+  padding: 5px;
   .settings-title {
     margin: 10px auto;
     text-align: center;
+  }
+  .settings-form-container {
+    padding: 5px;
+    .settings-subtitle {
+      font-size: 16px;
+    }
   }
 }
 </style>
