703b55dbb0e9b2a6fb1a300655188430ab0a2cd7
tigrig <tigrig.crex@gmail.com>
2019-05-09 14:19:44 +0900

    【convert】大まかなエリア作成

--- components/ConvertArea.vue
+++ components/ConvertArea.vue
@@ -1,5 +1,18 @@
 <template>
-  <div></div>
+  <section class="main-container">
+    <div class="mode-switch-wrapper"></div>
+    <ConvertContainer />
+  </section>
 </template>
+
+<script>
+import ConvertContainer from '@/components/main/ConvertContainer.vue'
+
+export default {
+  components: {
+    ConvertContainer
+  }
+}
+</script>
 
 <style></style>
---
+++ components/main/ConvertContainer.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="convert-container">
+    <div v-if="true" class="convert-file-area"></div>
+    <div v-if="false" class="convert-text-area"></div>
+  </section>
+</template>
+
+<style></style>
