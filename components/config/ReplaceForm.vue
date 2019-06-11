cb8e98cfe8bf07446fbd7e14cbfb0a3a8e220b2a
TigRig <tigrig.crex@gmail.com>
2019-06-11 17:54:21 +0900

    [add] Insert/ReplaceForm に vue-draggable 追加
    コンフィグ内の各設定行をドラッグ移動できるようになった

--- components/config/ReplaceForm.vue
+++ components/config/ReplaceForm.vue
@@ -1,66 +1,68 @@
 <template>
   <div class="ReplaceForm">
-    <div
-      v-for="setting in config.settings"
-      :key="setting.id"
-      class="ReplaceForm__Item"
-      :class="{
-        'ReplaceForm--enabled': setting.enabled,
-        'ReplaceForm--disabled': !setting.enabled
-      }"
-    >
-      <!-- スイッチ -->
-      <div class="ReplaceForm__Item__Switch">
-        <ScriponSwitch
-          :id="setting.id"
-          :enabled="setting.enabled"
-          @change="toggleSettingEnabled(setting)"
-        />
+    <draggable>
+      <div
+        v-for="setting in config.settings"
+        :key="setting.id"
+        class="ReplaceForm__Item"
+        :class="{
+          'ReplaceForm--enabled': setting.enabled,
+          'ReplaceForm--disabled': !setting.enabled
+        }"
+      >
+        <!-- スイッチ -->
+        <div class="ReplaceForm__Item__Switch">
+          <ScriponSwitch
+            :id="setting.id"
+            :enabled="setting.enabled"
+            @change="toggleSettingEnabled(setting)"
+          />
+        </div>
+        <!-- 入力エリア -->
+        <div class="ReplaceForm__Item__Textarea">
+          <ScriponTextarea
+            :horizontal="true"
+            :value="setting.value.before"
+            :placeholder="setting.placeholder.before"
+            :enabled="setting.enabled"
+            @keydown.enter.prevent=""
+            @input="
+              e => {
+                updateSetting({
+                  setting,
+                  value: { before: e.target.value, after: setting.value.after }
+                })
+              }
+            "
+          />
+          <div class="spacer--xs" />
+          <ScriponTextarea
+            :horizontal="true"
+            :value="setting.value.after"
+            :placeholder="setting.placeholder.after"
+            :enabled="setting.enabled"
+            @keydown.enter.prevent=""
+            @input="
+              e => {
+                updateSetting({
+                  setting,
+                  value: { before: setting.value.before, after: e.target.value }
+                })
+              }
+            "
+          />
+        </div>
+        <!-- 削除ボタン -->
+        <div class="ReplaceForm__Item__Button">
+          <scripon-button
+            caption="削除"
+            :danger="true"
+            :enabled="setting.enabled"
+            @click="deleteSetting(setting)"
+          />
+        </div>
       </div>
-      <!-- 入力エリア -->
-      <div class="ReplaceForm__Item__Textarea">
-        <ScriponTextarea
-          :horizontal="true"
-          :value="setting.value.before"
-          :placeholder="setting.placeholder.before"
-          :enabled="setting.enabled"
-          @keydown.enter.prevent=""
-          @input="
-            e => {
-              updateSetting({
-                setting,
-                value: { before: e.target.value, after: setting.value.after }
-              })
-            }
-          "
-        />
-        <div class="spacer--xs" />
-        <ScriponTextarea
-          :horizontal="true"
-          :value="setting.value.after"
-          :placeholder="setting.placeholder.after"
-          :enabled="setting.enabled"
-          @keydown.enter.prevent=""
-          @input="
-            e => {
-              updateSetting({
-                setting,
-                value: { before: setting.value.before, after: e.target.value }
-              })
-            }
-          "
-        />
-      </div>
-      <!-- 削除ボタン -->
-      <div class="ReplaceForm__Item__Button">
-        <scripon-button
-          caption="削除"
-          :danger="true"
-          :enabled="setting.enabled"
-          @click="deleteSetting(setting)"
-        />
-      </div>
-    </div>
+    </draggable>
     <!-- 追加ボタン -->
     <scripon-button
       caption="追加"
@@ -71,13 +73,16 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import { mapMutations } from 'vuex'
+
 import ScriponButton from '~/components/forms/ScriponButton'
 import ScriponTextarea from '~/components/forms/ScriponTextarea'
 import ScriponSwitch from '~/components/forms/ScriponSwitch'
 
 export default {
   components: {
+    draggable,
     ScriponButton,
     ScriponTextarea,
     ScriponSwitch
